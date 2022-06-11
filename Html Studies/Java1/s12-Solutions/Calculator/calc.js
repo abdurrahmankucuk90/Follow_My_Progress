8b2a9f58dd095acf46af8c4f117815c9236572bb
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-11 11:17:15 +0200

    asdf

--- Html Studies/Java1/s12-Solutions/Calculator/calc.js
+++ Html Studies/Java1/s12-Solutions/Calculator/calc.js
@@ -1,3 +1,33 @@
-const prevDisplay = document.querySelector(".previous-display")
-const currDisplay = document.querySelector(".current-display")
+//* =================================================
+//*                     IOS CALCULATOR
+//* =================================================
+const prevDisp = document.querySelector('.previous-display');
+const currDisp = document.querySelector('.current-display');
 
+const btnContainer = document.querySelector('.buttons-container');
+
+let currOperand = '';
+let previousOperand = '';
+
+//? Butonlari tasiyan container icin event tanimlamasi
+btnContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('num')) {
+    appendNumber(e.target.textContent);
+    updateDisplay();
+  }
+});
+
+const appendNumber = (num) => {
+  //? Eger ilk olarak  0 girilmisse geri don
+  if (!currOperand && num === '0') return;
+
+  //? Eğer şu anki sayi . ise ve önceki girilen sayi . iceriyorsa geri don
+  if (num === '.' && currOperand.includes('.')) return;
+
+  //? Girilen sayilari birlestir.
+  currOperand += num;
+};
+
+const updateDisplay = () => {
+  currDisp.textContent = currOperand;
+};
