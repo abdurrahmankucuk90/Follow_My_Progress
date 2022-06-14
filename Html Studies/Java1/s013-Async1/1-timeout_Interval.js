6b26ec6d4d65b373cf6953ba682036439ce81899
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-14 20:55:40 +0200

    asd

--- Html Studies/Java1/s013-Async1/1-timeout_Interval.js
+++ Html Studies/Java1/s013-Async1/1-timeout_Interval.js
@@ -82,15 +82,24 @@ let minutter = 0
 //* yapisinin kullanilmasi gerekebilir. Fakta bu iyi bir programlama yaklasimi degildir.
 // !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
 
-setTimeout(() => {
-    console.log("john:Hi");
-    setTimeout(() => {
-      console.log("Sarah: Hello");
-      setTimeout(() => {
-        console.log("John: How Are you?");
-        setTimeout(() => {
-          console.log("Fine and you?");
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
+// setTimeout(() => {
+//     console.log("john:Hi");
+//     setTimeout(() => {
+//       console.log("Sarah: Hello");
+//       setTimeout(() => {
+//         console.log("John: How Are you?");
+//         setTimeout(() => {
+//           console.log("Fine and you?");
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+  
+
+//? COZUMLER:
+//?----------------------------------------------------------------
+//* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+//? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//* 2- Promise,
+//! 3- Fetch API (Promise'in basitlestirilmis hali),
+//! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
