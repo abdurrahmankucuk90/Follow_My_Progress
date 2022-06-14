0fe8d18b78a2cfd9a7706c5bc253b6f3d3e6b5c3
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-14 22:36:27 +0200

    afs

--- Html Studies/Java1/s013-Async1/3-fetchAPI.js
+++ Html Studies/Java1/s013-Async1/3-fetchAPI.js
@@ -10,4 +10,26 @@
 //? fetch() fonksiyonu veri getirmek istediginiz kaynagin yolunu gosteren zorunlu
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
 
+fetch("https://api.github.com/users")
+  .then((res) => {
+    //!Eror handling
+    if (!res.ok) {
+      throw new Error(`Something went wrong: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then((data) => updateDom(data))
+  .catch((err) => console.log(err));
+
+//console.log(dataFromAPI); //* Microda kaldigindan ise yaramadi, senkron cunku
+const updateDom = (data) => {
+  const userDiv = document.querySelector(".users");
+  data.forEach((x) => {
+    const{login, avatar_url,html_url} =x
+    userDiv.innerHTML += `<h2 class="text-warning">NAME:${login}</h2>
+    <img src=${avatar_url} alt=""/>
+    <p>${html_url}</p>
+    `;
+  });
+};
 
