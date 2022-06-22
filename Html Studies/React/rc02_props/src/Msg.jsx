a5b2c6c7c6e73d7c01ad9271debc6267a3affcd8
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-22 21:56:22 +0200

    asd

---
+++ Html Studies/React/rc02_props/src/Msg.jsx
@@ -0,0 +1,11 @@
+const Msg = (probs) => {
+  //! Destructuring
+  const { isim } = probs;
+  return (
+    <div>
+      <h3>Merhaba {isim}</h3>
+    </div>
+  );
+};
+
+export default Msg;
---
+++ Html Studies/React/rc02_props/src/Person.jsx
@@ -0,0 +1,15 @@
+import Msg from './Msg';
+
+const Person = ({ name, img, tel }) => {
+  // const {name, img, tel } = probs
+  return (
+    <div>
+      {/* <h1>Merhaba {name}</h1> */}
+      <Msg isim = {name}/>
+      <img src={img} alt={name} />
+      <p>{tel}</p>
+    </div>
+  );
+};
+
+export default Person;
