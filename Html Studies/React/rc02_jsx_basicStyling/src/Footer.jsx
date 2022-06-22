0e8237a34a4e6dffaccd9452c351b150d34e2a71
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-22 20:00:07 +0200

    sfd

---
+++ Html Studies/React/rc02_jsx_basicStyling/src/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer>Copyright by Clarusway, {new Date().getFullYear()}</footer>;
+};
+
+export default Footer;
---
+++ Html Studies/React/rc02_jsx_basicStyling/src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header>
+            <h1>Headers</h1>
+        </header>
+    )
+}
+
+export default Header;
