3a267d45dfadf035ba568081c28b2ecbdff4ffa5
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-27 20:57:22 +0200

    asd

--- Html Studies/React/rc05_events-useStateHook/src/App.js
+++ Html Studies/React/rc05_events-useStateHook/src/App.js
@@ -1,9 +1,11 @@
-import Event from "./components/Event";
+import Event from "./components/1-event-intro/Event";
+import Counter from "./components/2-classCompanents/Counter";
 
 function App() {
   return (
     <div>
       <Event/>
+      <Counter/>
     </div>
   );
 }
