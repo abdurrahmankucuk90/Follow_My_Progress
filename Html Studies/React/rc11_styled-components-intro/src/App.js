45c3982f9d762dc2b159d55167d8900e7d06aeb9
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-07-05 20:25:07 +0200

    lnik

--- Html Studies/React/rc11_styled-components-intro/src/App.js
+++ Html Studies/React/rc11_styled-components-intro/src/App.js
@@ -1,16 +1,24 @@
 import Button, { TomatoButton } from "./components/Button.styled";
 import Container from "./components/Container.styled";
 import HeaderText from "./components/Header.styled";
+import StyledLink from "./components/Link.styled";
 
 const App = () => {
   return (
-    <Container bg='green' >
+    <Container bg="green">
       <HeaderText>Styled-Components</HeaderText>
       <Button primary>Click</Button>
       <Button>Click</Button>
       <TomatoButton primary> Click </TomatoButton>
       <TomatoButton> Click </TomatoButton>
-      <HeaderText color='white'>Styled-Components</HeaderText>
+      <HeaderText color="white">Styled-Components</HeaderText>
+
+      <StyledLink href="https://www.clarusway.com" target="_blank">
+        Clarusway
+      </StyledLink>
+      <StyledLink secondary href="https://www.clarusway.com" target="_blank">
+        Clarusway
+      </StyledLink>
       {/* <Button/> */}
     </Container>
   );
