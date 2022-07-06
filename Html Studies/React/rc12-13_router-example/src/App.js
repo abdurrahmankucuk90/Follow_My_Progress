20ee47b4ce23ef3cb4d63502696f71d9651b33a6
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-07-06 22:06:21 +0200

    dopne

--- Html Studies/React/rc12-13_router-example/src/App.js
+++ Html Studies/React/rc12-13_router-example/src/App.js
@@ -1,20 +1,29 @@
-import Footer from './components/Footer';
-import MyNavbar from './components/Nav';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Instructors from './pages/Instructors';
-import NotFound from './pages/NotFound';
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InstructorsDetail from './pages/InstructorsDetail'
 
 function App() {
   return (
-    <>
-      <MyNavbar />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Home /> */}
+        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/instructors/:id" element={<InstructorsDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+        {/* <Instructors /> */}
+        {/* <Contact /> */}
+        {/* <NotFound /> */}
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- Html Studies/React/rc12_router-intro/src/App.js
+++ Html Studies/React/rc12_router-intro/src/App.js
@@ -4,16 +4,22 @@ import ContactForm from "./pages/Contact";
 import Home from "./pages/Home";
 import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="container">
-      <Nav />
-      <Home />
-      <Instructors />
-      <ContactForm />
-      <NotFound />
-      <Footer />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="instructors" element={<Instructors />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
