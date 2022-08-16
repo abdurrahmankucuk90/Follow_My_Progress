df1563252643f9e41fa12eda73f2b3aad9933eb1
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-08-16 21:50:06 +0200

    a

--- Back-End/cl02/oop.py
+++ Back-End/cl02/oop.py
@@ -1,12 +1,28 @@
-import os
-os.system('cls' if os.name == 'nt' else 'clear')
+class Person:
+    company = "clarusway"
+
+    def test(self):
+        print("test")
+
+    def set_details(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_details(self):
+        print(self.name, self.age)
 
 
+@staticmethod
+def salute():
+    print("Hi there!")
 
 
+person1 = Person()
+person2 = Person()
 
+# person1.test()
+# Person.test()
 
-
-
-
-
+person1.set_details("barry", 20)
+person1.get_details()
+print(person1.name)
