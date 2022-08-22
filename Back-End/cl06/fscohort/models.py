b88330b6f3002cd7a6d07a93b120eb893a782a5a
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-08-22 21:33:12 +0200

    relations

--- Back-End/cl06/fscohort/models.py
+++ Back-End/cl06/fscohort/models.py
@@ -2,7 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+    COUNTRIES = [
+        ('TR','Turkey'),
+        ('US', 'America'),
+        ('DE', 'Germany'),
+        ('FR', 'France')
+    ]
     first_name = models.CharField('Adi', max_length=50)
     second_name = models.CharField('Soyadi', max_length=50)
     # full_name = first_name + " " + second_name
     number = models.IntegerField("Numara")
+    about = models.TextField('Hakkinda', blank=True, null=True)
+    country = models.CharField('Ulke', max_length=2, choices = COUNTRIES, default='TR')
+    avatar = models.ImageField('Resim',blank=True, null = True , upload_to='media/')
+    registered_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField( auto_now=True)
+
+    def __str__(self):
+        return f'{self.number} {self.first_name} {self.second_name}'
+    
+    
+    class Meta:
+        ordering = ['number']
--- Back-End/cl06/main/urls.py
+++ Back-End/cl06/main/urls.py
@@ -21,3 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),  
     path('fscohort/', include('fscohort.urls')), #=> fscohort/urls.py
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
