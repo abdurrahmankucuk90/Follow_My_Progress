ad044061864ec979f08ef090dba3d7c3f93e086e
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-08-22 21:42:58 +0200

    s

--- Back-End/cl06/relations/admin.py
+++ Back-End/cl06/relations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from relations.models import Creator
+
 # Register your models here.
+from .models import *
+admin.site.register(Creator)
+admin.site.register(Language)
--- Back-End/cl06/relations/models.py
+++ Back-End/cl06/relations/models.py
@@ -10,6 +10,9 @@ class Creator(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+    class Meta:
+        ordering = ['first_name']
+
 # one2one relation:
 class Language(models.Model):
     name = models.CharField(max_length=50)
@@ -17,3 +20,14 @@ class Language(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.creator}"
+
+        '''
+on_delete properties:
+
+# CASCADE -> if primary deleted, delete foreing too.
+# SET_NULL -> if primary deleted, set foreign to NULL. (null=True)
+# SET_DEFAULT -> if primary deleted, set foreing to DEFAULT value. (default='Value")
+# DO NOTHING -> if primary deleted, do nothing.
+# PROTECT -> if foreign is exist, can not delete primary.
+
+'''
