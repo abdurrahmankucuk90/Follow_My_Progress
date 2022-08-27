870955fdb521540ecebe3de421a894fb65a2d39a
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-08-27 10:19:35 +0200

    sdf

--- Back-End/cl09/fscohort/urls.py
+++ Back-End/cl09/fscohort/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('list/', student_list, name='list'),
+    path('add/', student_add, name='Add Student'),
+    path('update/<int:id>', student_update, name='update'),
+    path('delete/<int:id>', student_delete, name='delete'),
+    path('student/<int:id>', student_detail, name="detail"),
+
     
 ]
--- Html Assignments/hc-ass3/assigment/README.md
+++
@@ -1,3 +0,0 @@
-## ASSIGMENT-3
-1-) Ekran ciktisi görünen Sol Açılır menüyü transition kullanarak yapınız.
-2-) Ekran çıktısı görününen nav'ı Flexbox kullanarak yapınız.
